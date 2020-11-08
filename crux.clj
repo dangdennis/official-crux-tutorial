bc82d9d47762da4bb2b13eeff4048b685203c926
Dennis Dang <dangggdennis@gmail.com>
2020-11-08 00:44:08 -0500

    chapter 3 done

--- crux.clj
+++ crux.clj
@@ -3,84 +3,7 @@
 (def crux
   (crux/start-node {}))
 
-(def manifest
-  {:crux.db/id :manifest
-   :pilot-name "Johanna"
-   :id/rocket "SB002-sol"
-   :id/employee "22910x2"
-   :badges "SETUP"
-   :cargo ["stereo" "gold fish" "slippers" "secret note"]})
-
-(crux/submit-tx crux [[:crux.tx/put manifest]])
-
-(crux/entity (crux/db crux) :manifest)
-
-(crux/submit-tx crux [[:crux.tx/put {:crux.db/id :commodity/Pu
-                                     :common-name "Plutonium"
-                                     :type :element/metal
-                                     :density 19.816
-                                     :radioactive true}]
-
-                      [:crux.tx/put {:crux.db/id :commodity/N
-                                     :common-name "Nitrogen"
-                                     :type :element/gas
-                                     :density 1.2506}]
-
-                      [:crux.tx/put {:crux.db/id :commodity/CH4
-                                     :common-name "Methane"
-                                     :type :molecule/gas
-                                     :density 0.717
-                                     :radioactive false}]])
-
-(crux/submit-tx crux
-                [[:crux.tx/put
-                  {:crux.db/id :stock/Pu
-                   :commod :commodity/Pu
-                   :weight-ton 21}
-                  #inst "2115-02-13T18"]
-
-                 [:crux.tx/put
-                  {:crux.db/id :stock/Pu
-                   :commod :commodity/Pu
-                   :weight-ton 23}
-                  #inst "2115-02-14T18"]
-
-                 [:crux.tx/put
-                  {:crux.db/id :stock/Pu
-                   :commod :commodity/Pu
-                   :weight-ton 22.2}
-                  #inst "2115-02-15T18"]
-
-                 [:crux.tx/put
-                  {:crux.db/id :stock/Pu
-                   :commod :commodity/Pu
-                   :weight-ton 24}
-                  #inst "2115-02-18T18"]
-
-                 [:crux.tx/put
-                  {:crux.db/id :stock/Pu
-                   :commod :commodity/Pu
-                   :weight-ton 24.9}
-                  #inst "2115-02-19T18"]])
-
-(crux/submit-tx crux
-                [[:crux.tx/put
-                  {:crux.db/id :stock/N
-                   :commod :commodity/N
-                   :weight-ton 3}
-                  #inst "2115-02-13T18"
-                  #inst "2115-02-19T18"]
-
-                 [:crux.tx/put
-                  {:crux.db/id :stock/CH4
-                   :commod :commodity/CH4
-                   :weight-ton 92}
-                  #inst "2115-02-15T18"
-                  #inst "2115-02-19T18"]])
-
-(crux/entity (crux/db crux #inst "2115-02-14") :stock/Pu)
-
-(crux/entity (crux/db crux #inst "2115-02-18") :stock/Pu)
+;; Chapter 3 Mercury
 
 (defn easy-ingest
   "Uses Crux put transaction to add a vector of documents to a specified
@@ -90,10 +13,110 @@
                   (vec (for [doc docs]
                          [:crux.tx/put doc]))))
 
-(crux/submit-tx crux [[:crux.tx/put {:crux.db/id :manifest
-                                     :pilot-name "Johanna"
-                                     :id/rocket "SB002-sol"
-                                     :id/employee "22910x2"
-                                     :badges ["SETUP", "PUT"]
-                                     :cargo ["stereo" "gold fish" "slippers" "secret note"]}]])
+(def data
+  [{:crux.db/id :commodity/Pu
+    :common-name "Plutonium"
+    :type :element/metal
+    :density 19.816
+    :radioactive true}
 
+   {:crux.db/id :commodity/N
+    :common-name "Nitrogen"
+    :type :element/gas
+    :density 1.2506
+    :radioactive false}
+
+   {:crux.db/id :commodity/CH4
+    :common-name "Methane"
+    :type :molecule/gas
+    :density 0.717
+    :radioactive false}
+
+   {:crux.db/id :commodity/Au
+    :common-name "Gold"
+    :type :element/metal
+    :density 19.300
+    :radioactive false}
+
+   {:crux.db/id :commodity/C
+    :common-name "Carbon"
+    :type :element/non-metal
+    :density 2.267
+    :radioactive false}
+
+   {:crux.db/id :commodity/borax
+    :common-name "Borax"
+    :IUPAC-name "Sodium tetraborate decahydrate"
+    :other-names ["Borax decahydrate" "sodium borate" "sodium tetraborate" "disodium tetraborate"]
+    :type :mineral/solid
+    :appearance "white solid"
+    :density 1.73
+    :radioactive false}])
+
+
+(easy-ingest crux data)
+
+(crux/q (crux/db crux)
+        '{:find [element]
+          :where [[element :type :element/metal]]})
+
+(=
+ (crux/q (crux/db crux)
+         '{:find [element]
+           :where [[element :type :element/metal]]})
+
+ (crux/q (crux/db crux)
+         {:find '[element]
+          :where '[[element :type :element/metal]]})
+
+ (crux/q (crux/db crux)
+         (quote
+          {:find [element]
+           :where [[element :type :element/metal]]})))
+
+(crux/q (crux/db crux)
+        '{:find [name]
+          :where [[e :type :element/metal]
+                  [e :common-name name]]})
+
+(crux/q (crux/db crux)
+        '{:find [name rho]
+          :where [[e :density rho]
+                  [e :common-name name]]})
+
+(crux/q (crux/db crux)
+        '{:find [name]
+          :where [[e :type t]
+                  [e :common-name name]]
+          :args [{t :element/metal}]})
+
+(defn filter-type
+  [type]
+  (crux/q (crux/db crux)
+          {:find '[name]
+           :where '[[e :type t]
+                    [e :common-name name]]
+           :args [{'t type}]}))
+
+(defn filter-appearance
+  [description]
+  (crux/q (crux/db crux)
+          {:find '[name IUPAC]
+           :where '[[e :common-name name]
+                    [e :IUPAC-name IUPAC]
+                    [e :appearance ?appearance]]
+           :args [{'?appearance description}]}))
+
+(filter-type :element/metal)
+
+(filter-appearance "white solid")
+
+(crux/submit-tx
+ crux
+ [[:crux.tx/put
+   {:crux.db/id :manifest
+    :pilot-name "Johanna"
+    :id/rocket "SB002-sol"
+    :id/employee "22910x2"
+    :badges ["SETUP" "PUT" "DATALOG-QUERIES"]
+    :cargo ["stereo" "gold fish" "slippers" "secret note"]}]])
